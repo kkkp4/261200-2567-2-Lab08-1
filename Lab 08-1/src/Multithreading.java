fd20e3c88ab8ef153b3b84b0feb5614af4fbca69
kkkp4 <152593844+kkkp4@users.noreply.github.com>
2025-02-05 15:58:13 +0700

    Add files via upload

---
+++ Lab 08-1/src/Multithreading.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+class NumberPrinter extends Thread {
+    private int threadNumber;
+    private static final Random random = new Random();
+
+    public NumberPrinter(int threadNumber) {
+        this.threadNumber = threadNumber;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 1; i <= 50; i++) {
+            System.out.println("Thread #" + threadNumber + ": " + i);
+
+            // Adding a random delay to simulate real-time behavior
+            try {
+                Thread.sleep(1000); // Random sleep between 0-99 ms
+            } catch (InterruptedException e) {
+                return;  // stop timer
+            }
+        }
+    }
+}
